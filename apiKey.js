a3f52ae13c674b515c7db38799b532ffec574e62
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-04-15 12:12:57 -0700

    ee

--- apiKey.js
+++ apiKey.js
@@ -1,4 +1,4 @@
-console.log('')
+console.log('0110010001101111011100000110010101100110011010010111001101101000')
 function getApiKey() {
   return 'Ibx12qSDWa4FVJ8s59FytPV9MD7QAza90kTzHGha'
 }
--- getDatesAndDurations.js
+++ getDatesAndDurations.js
@@ -3,3 +3,8 @@ function getDatesAndDurations(landmark) {
   document.getElementById("years-of-personal-experience").innerHTML = 15
   document.getElementById("age-of-amelie").innerHTML = '9-month-old'
 }
+
+function ee() {
+  console.log('oremor nhoj em llik tsum uoy emag eht niw ot')
+  document.getElementsByTagName("header")[0].style.backgroundImage = "url('/assets/doom.jpg')";
+}
