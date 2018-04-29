4eb23319a52442900b4e95f31e8f67f7fcf2b4e0
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-04-28 18:57:12 -0700

    Update getDurations.js

--- getDurations.js
+++ getDurations.js
@@ -1,16 +1,25 @@
 function getDurations () {
   
-  const getMillisecondsSince = function (start) {
-    return ((new Date ()).getTime() - new Date (start, 0, 1))
+  const getMillisecondsSince = function (year, month=0) {
+    return ((new Date ()).getTime() - new Date (year, month, 1))
   }
   
-  const getYearsSince = function (start, round=1) {
-    return round * Math.floor(1/round * Math.floor(getMillisecondsSince(start) / (1000 * 60 * 60 * 24 * 7 * 52)))
+  const getYearsSince = function (year, round=1) {
+    return round * Math.floor(1/round * Math.floor(getMillisecondsSince(year) / (1000 * 60 * 60 * 24 * 7 * 52)))
+  }
+  
+  const getMonthsSince(year, month) {
+    return Math.floor(getMillisecondsSince(year, month) / (1000 * 60 * 60 * 24 * 30))
+  }
+  
+  const getAmeliesAge = function () {
+    const months = getMonthsSince(2017, 5)
+    return months < 12 ? `${months}-month-old` : `${getYearsSince(2017)}-year-old`
   }
   
   document.getElementById("years-of-professional-experience").innerHTML = getYearsSince(2015)
-  document.getElementById("years-of-personal-experience").innerHTML = getYearsSince(2000, 5)
-  document.getElementById("age-of-amelie").innerHTML = '9-month-old'
+  document.getElementById("years-of-personal-experience").innerHTML = getYearsSince(2003, 5)
+  document.getElementById("age-of-amelie").innerHTML = getAmeliesAge()
 }
 
 function ee() {
