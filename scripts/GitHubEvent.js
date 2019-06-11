b3a809c5592f6985a24621465823a7043daaeee9
Yves Gurcan <gurcan.yves+github@gmail.com>
2019-06-10 23:36:38 -0700

    add github public activity wall

---
+++ scripts/GitHubEvent.js
@@ -0,0 +1,240 @@
+const GITHUB_ICON_PATH_PREFIX = '/assets/github/';
+
+class GitHubEvent {
+    constructor(event, index) {
+        this.data = event;
+        this.index = index;
+
+        this.id = event.id;
+        this.title = `${this.getName()}.`;
+        this.icon = this.getIcon();
+        this.link = this.getLink();
+        this.repo = {
+            name: event.repo.name,
+            apiUrl: event.repo.url
+        };
+    }
+
+    get timeFromNow() {
+        const createdAt = this.data.created_at;
+        return moment(createdAt).fromNow();
+    }
+
+    getName() {
+        const { type, payload = {} } = this.data;
+        const {
+            action,
+            ref_type,
+            pages,
+            commits,
+            issue = {},
+            pull_request = {}
+        } = payload;
+        const { pull_request: issue_pull_request } = issue;
+        const { merged_at } = pull_request;
+        switch (type) {
+            default: {
+                return '';
+            }
+            case 'CommitCommentEvent': {
+                return 'left a comment on a commit';
+            }
+            case 'CreateEvent': {
+                return `created a ${ref_type}`;
+            }
+            case 'DeleteEvent': {
+                return `deleted a ${ref_type}`;
+            }
+            case 'ForkEvent': {
+                return 'forked a repository';
+            }
+            case 'GollumEvent': {
+                return `${pages[0].action} a wiki page`;
+            }
+            case 'IssueCommentEvent': {
+                if (issue_pull_request) {
+                    return 'commented on a pull request';
+                }
+
+                // 'edited' and 'deleted' are not public
+                return 'commented on an issue';
+            }
+            case 'IssuesEvent': {
+                return `${action} an issue`;
+            }
+            case 'PullRequestEvent': {
+                // TODO: there might be some other public actions whose wording does not work very well like that
+                const prAction = merged_at ? 'merged' : action;
+
+                return `${prAction} a pull request`;
+            }
+            case 'PullRequestReviewEvent': {
+                // need to be tested
+                return `${action} a review on a pull request`;
+            }
+            case 'PullRequestReviewCommentEvent': {
+                // 'edited' and 'deleted' are not public
+                return `commented on a pull request`;
+            }
+            case 'PushEvent': {
+                const commitCount = commits.length;
+                return `pushed ${commitCount} commit${
+                    commitCount > 1 ? 's' : ''
+                    }`;
+            }
+            case 'WatchEvent': {
+                return `${action} watching a repository`;
+            }
+        }
+    }
+
+    getIcon() {
+        let path = GITHUB_ICON_PATH_PREFIX;
+        const { type, payload = {} } = this.data;
+        const { action, ref_type, pull_request = {} } = payload;
+        const { merged_at } = pull_request;
+        switch (type) {
+            default: {
+                return '';
+            }
+            case 'CreateEvent': {
+                switch (ref_type) {
+                    default: {
+                        return '';
+                    }
+                    case 'branch': {
+                        path += 'git-branch';
+                        break;
+                    }
+                    case 'repository': {
+                        path += 'repo';
+                        break;
+                    }
+                }
+                break;
+            }
+            case 'DeleteEvent': {
+                path += 'trashcan';
+                break;
+            }
+            case 'CommitCommentEvent':
+            case 'IssueCommentEvent':
+            case 'PullRequestReviewCommentEvent': {
+                path += 'comment';
+                break;
+            }
+            case 'ForkEvent': {
+                path += 'repo-forked';
+                break;
+            }
+            case 'GollumEvent': {
+                path += 'book';
+                break;
+            }
+            case 'IssuesEvent': {
+                switch (action) {
+                    default: {
+                        path += 'issue-opened';
+                        break;
+                    }
+                    case 'reopened': {
+                        path += 'issue-reopened';
+                        break;
+                    }
+                    case 'closed': {
+                        path += 'issue-closed';
+                        break;
+                    }
+                }
+                break;
+            }
+            case 'PullRequestEvent': {
+                switch (action) {
+                    default: {
+                        path += 'git-pull-request';
+                        break;
+                    }
+                    case 'closed': {
+                        if (merged_at) {
+                            path += 'git-merge';
+                            break;
+                        }
+
+                        path += 'circle-slash';
+                        break;
+                    }
+                }
+                break;
+            }
+            case 'PushEvent': {
+                path += 'repo-push';
+                break;
+            }
+            case 'WatchEvent': {
+                path += 'eye';
+                break;
+            }
+        }
+
+        return `${path}.svg`;
+    }
+
+    getLink() {
+        const { type, payload = {} } = this.data;
+        const {
+            action,
+            ref_type,
+            pull_request = {},
+            issue = {},
+            comment = {}
+        } = payload;
+        const { html_url: prUrl } = pull_request;
+        const { html_url: issueUrl } = issue;
+        const { html_url: commentUrl } = comment;
+        switch (type) {
+            default: {
+                return '';
+            }
+            case 'IssueCommentEvent': {
+                return commentUrl;
+            }
+            case 'IssuesEvent': {
+                return issueUrl;
+            }
+            case 'PullRequestEvent': {
+                return prUrl;
+            }
+        }
+    }
+
+    get linkIsSameAsRepoUrl() {
+        const { type, payload = {} } = this.data;
+        const { ref_type, ref, forkee = {} } = payload;
+        const { html_url } = forkee;
+
+        switch (type) {
+            default: {
+                return false;
+            }
+            case 'CreateEvent': {
+                switch (ref_type) {
+                    default: {
+                        return false;
+                    }
+                    case 'branch': {
+                        return `/tree/${ref}`;
+                    }
+                    case 'repository': {
+                        return true;
+                    }
+                }
+            }
+            case 'ForkEvent': {
+                return html_url;
+            }
+            case 'WatchEvent': {
+                return true;
+            }
+        }
+    }
+}
---
+++ scripts/handle_github_events.js
@@ -0,0 +1,172 @@
+const githubReposInEvents = [];
+const githubEventsWithRepoLinks = {};
+
+const addDeferredRepoRequest = event => {
+    const { repo = {}, id: eventId, linkIsSameAsRepoUrl } = event;
+    const { apiUrl: url } = repo;
+    const deferredRequestIndex = githubReposInEvents.findIndex(
+        repoEvents => repoEvents.url === url
+    );
+
+    if (linkIsSameAsRepoUrl) {
+        githubEventsWithRepoLinks[eventId] = linkIsSameAsRepoUrl;
+    }
+
+    if (deferredRequestIndex > -1) {
+        const deferredRequest = githubReposInEvents[deferredRequestIndex];
+        githubReposInEvents[deferredRequestIndex] = {
+            ...deferredRequest,
+            eventIds: [...deferredRequest.eventIds, eventId]
+        };
+        return;
+    }
+
+    githubReposInEvents.push({ url, eventIds: [eventId] });
+};
+
+const handleRepoUrls = async () => {
+    githubReposInEvents.forEach(async repoEvents => {
+        try {
+            const result = await fetch(repoEvents.url);
+            if (result) {
+                const data = await result.json();
+                if (data && data.html_url) {
+                    repoEvents.eventIds.map(eventId => {
+                        const repoElem = document.getElementById(
+                            `event-repo${eventId}`
+                        );
+                        if (repoElem) {
+                            repoElem.setAttribute('href', data.html_url);
+                            repoElem.setAttribute('target', '_blank');
+                            repoElem.setAttribute('rel', 'noopener');
+                        }
+
+                        const eventLink = githubEventsWithRepoLinks[eventId];
+                        if (eventLink) {
+                            const titleElem = document.getElementById(
+                                `event-title${eventId}`
+                            );
+
+                            if (titleElem) {
+                                let url = data.html_url;
+                                if (eventLink.indexOf('https') === 0) {
+                                    url = eventLink;
+                                } else if (eventLink !== true) {
+                                    url += eventLink;
+                                }
+
+                                titleElem.setAttribute('href', url);
+                                titleElem.setAttribute('target', '_blank');
+                                titleElem.setAttribute('rel', 'noopener');
+                            }
+                        }
+                    });
+                }
+            }
+        } catch (error) {
+            console.error({ url: repoEvents.url, error });
+        }
+    });
+};
+
+(async function () {
+    const EVENT_LIST_ELEMENT_ID = 'social-wall';
+    const EVENT_CLASS = 'event';
+    const GITHUB_USERNAME = 'yvesgurcan';
+    const USER_PUBLIC_ACTIVITY_ENDPOINT = `https://api.github.com/users/${GITHUB_USERNAME}/events/public`;
+
+    const eventList = document.getElementById(EVENT_LIST_ELEMENT_ID);
+
+    if (eventList) {
+        const publicActivityResponse = await fetch(
+            USER_PUBLIC_ACTIVITY_ENDPOINT
+        );
+
+        if (publicActivityResponse.status !== 200) {
+            return;
+        }
+
+        const publicActivity = await publicActivityResponse.json();
+
+        publicActivity.forEach((rawEvent, index) => {
+            if (index === 0) {
+                const activityContainer = document.getElementById('activity');
+                if (activityContainer) {
+                    const activityTitle = document.createElement('h4');
+                    activityContainer.prepend(activityTitle);
+
+                    const activityLink = document.createElement('a');
+                    activityLink.innerHTML = 'GitHub public activity';
+                    activityLink.href = '#activity';
+                    activityTitle.prepend(activityLink);
+                }
+            }
+
+            const event = new GitHubEvent(rawEvent, index);
+
+            // event details
+            const eventDetails = document.createElement('div');
+            eventDetails.classList = 'event-details';
+
+            const eventItemInner = document.createElement('div');
+            eventItemInner.classList = 'event-inner';
+
+            let eventName = document.createElement('a');
+
+            if (event.link) {
+                eventName.setAttribute('href', event.link);
+                eventName.setAttribute('target', '_blank');
+                eventName.setAttribute('rel', 'noopener');
+            }
+
+            eventName.id = `event-title${event.id}`;
+            eventName.classList = 'event-title';
+            eventName.innerHTML = event.title;
+            eventDetails.append(eventName);
+
+            const eventRepo = document.createElement('a');
+            eventRepo.id = `event-repo${event.id}`;
+            eventRepo.classList = 'event-repo';
+            eventRepo.innerHTML = event.repo.name;
+            eventDetails.append(eventRepo);
+
+            const eventTime = document.createElement('div');
+            eventTime.classList = 'event-time';
+            eventTime.innerHTML = event.timeFromNow;
+            eventDetails.append(eventTime);
+
+            // event image
+            if (event.icon) {
+                const eventIcon = document.createElement('div');
+                eventIcon.classList = 'event-icon';
+
+                const eventIconInner = document.createElement('img');
+                eventIconInner.classList = 'event-icon-inner';
+                eventIconInner.setAttribute('src', event.icon);
+
+                eventIcon.append(eventIconInner);
+                eventItemInner.append(eventIcon);
+            }
+
+            // group children elements
+            const eventItem = document.createElement('li');
+            eventItem.className = EVENT_CLASS;
+            eventItem.id = `event${event.id}`;
+
+            eventItemInner.append(eventDetails);
+            eventItem.append(eventItemInner);
+
+            eventList.append(eventItem);
+
+            // group requests for repo details to avoid sending duplicate requests concurrently
+            addDeferredRepoRequest(event);
+        });
+
+        // send all requests for repo details
+        handleRepoUrls();
+
+        console.log({ publicActivity });
+    } else {
+        console.error(`Could not find element #${ACTIVITY_LIST_ELEMENT_ID}.`);
+    }
+})();
